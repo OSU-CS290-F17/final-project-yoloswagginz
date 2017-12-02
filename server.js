65f61b91bb7408466a67336457267168c63caade
buitim <buitim@oregonstate.edu>
2017-12-02 00:18:13 -0800

    Re-Added MongoDB

--- server.js
+++ server.js
@@ -4,26 +4,31 @@ var exhbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
 
+// MongoDB //
+var MongoClient = require('mongodb').MongoClient;
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+
 app.engine('handlebars', exhbs({
-	defaultLayout: 'main'
+    defaultLayout: 'main'
 }));
 
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-	console.log("== Root page called.")
-	res.status(200).render('homePage');
+    console.log("== Root page called.")
+    res.status(200).render('homePage');
 });
 
 app.use(express.static('public'));
 
 app.get('*', function (req, res) {
 
-	console.log("== 404 page called.")
+    console.log("== 404 page called.")
 
-	res.status(404).render('404Page');
+    res.status(404).render('404Page');
 });
 
 app.listen(port, function () {
-	console.log("== Server is listening on port", port);
+    console.log("== Server is listening on port", port);
 });
