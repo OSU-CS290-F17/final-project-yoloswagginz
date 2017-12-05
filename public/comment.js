e2d8a2c94322e60f7fe07426b6ec0e304eedd3f2
buitim <buitim@oregonstate.edu>
2017-12-04 17:13:24 -0800

    testing different arrows

---
+++ public/comment.js
@@ -0,0 +1,12 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['comment'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<div class=\"comment\">\n	<div class=\"username\">"
+    + alias4(((helper = (helper = helpers.userName || (depth0 != null ? depth0.userName : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"userName","hash":{},"data":data}) : helper)))
+    + "</div>\n	<div class=\"comment-content\">"
+    + alias4(((helper = (helper = helpers.comment || (depth0 != null ? depth0.comment : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"comment","hash":{},"data":data}) : helper)))
+    + "</div>\n</div>";
+},"useData":true});
+})();
