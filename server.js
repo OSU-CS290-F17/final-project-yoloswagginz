ef887af1f07c62c7c7942d8e333412921d3648c5
buitim <buitim@oregonstate.edu>
2017-12-04 19:50:37 -0800

    did some shit

--- server.js
+++ server.js
@@ -1,13 +1,47 @@
+// var path = require('path');
+// var express = require('express');
+// var exhbs = require('express-handlebars');
+// var app = express();
+// var port = process.env.PORT || 3000;
+
+// // MongoDB //
+// var MongoClient = require('mongodb').MongoClient;
+// var mongoHost = process.env.MONGO_HOST;
+// var mongoPort = process.env.MONGO_PORT || 27017;
+// var mongoUser = process.env.MONGO_USER;
+// var mongoPassword = process.env.MONGO_PASSWORD;
+// var mongoDBName = process.env.MONGO_DB;
+
+// // Connection URL
+// var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword +
+//     '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+
+// var mongoConnection = null;
+
 var path = require('path');
 var express = require('express');
 var exhbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
 var app = express();
 var port = process.env.PORT || 3000;
 
-// MongoDB //
-var MongoClient = require('mongodb').MongoClient;
 var mongoHost = process.env.MONGO_HOST;
 var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB;
+
+// var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword +
+//     '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+
+var mongoURL = 'mongodb://localhost:27017/data';
+
+var mongoConnection = null;
+
+// console.log("== Mongo URL:", mongoURL);
+
 
 app.engine('handlebars', exhbs({
     defaultLayout: 'main'
@@ -29,6 +63,12 @@ app.get('*', function (req, res) {
     res.status(404).render('404Page');
 });
 
-app.listen(port, function () {
-    console.log("== Server is listening on port", port);
+MongoClient.connect(mongoURL, function (err, connection) {
+    if (err) {
+        throw err;
+    }
+    mongoConnection = connection;
+    app.listen(port, function () {
+        console.log("== Server listening on port:", port);
+    });
 });
