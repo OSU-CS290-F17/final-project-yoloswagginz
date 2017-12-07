1d00cf34c92213042080fb35e5e3d90fd39696f2
buitim <buitim@oregonstate.edu>
2017-12-07 02:27:27 -0800

    Changed spaces to tabs.

--- index.js
+++ index.js
@@ -16,101 +16,101 @@ var newcommentcontainer = document.getElementById('add-comment-container');
 var check;
 
 function show(show, hide, ele) {
-    show.classList.remove('masked');
-    show.classList.add('center');
-    hide.classList.add('masked');
-    if (ele) {
-        check = ele.parentElement.parentElement.parentElement; // Get the great grandparent of comment or dislike button
-    }
+	show.classList.remove('masked');
+	show.classList.add('center');
+	hide.classList.add('masked');
+	if (ele) {
+		check = ele.parentElement.parentElement.parentElement; // Get the great grandparent of comment or dislike button
+	}
 }
 
 function cancel(show, hide) {
-    hide.classList.add('masked');
-    hide.classList.remove('center');
-    show.classList.remove('masked');
-    clean();
+	hide.classList.add('masked');
+	hide.classList.remove('center');
+	show.classList.remove('masked');
+	clean();
 }
 
 function create() //Create new post; Needs template
 {
-    var title = document.getElementById('post-title-input').value; // Post Title
-    var textcontent = document.getElementById('post-content-input').value; // Post Content
-    if (title && textcontent) {
+	var title = document.getElementById('post-title-input').value; // Post Title
+	var textcontent = document.getElementById('post-content-input').value; // Post Content
+	if (title && textcontent) {
 
-        // Render new post into page here
-        cancel(content, newpostcontainer);
-    } else {
-        alert("You must fill all blanks!");
-    }
+		// Render new post into page here
+		cancel(content, newpostcontainer);
+	} else {
+		alert("You must fill all blanks!");
+	}
 }
 
 function clean() {
-    var all = document.getElementsByTagName('input');
-    for (var i = 0; i < all.length; i++) {
-        all[i].value = "";
-    }
+	var all = document.getElementsByTagName('input');
+	for (var i = 0; i < all.length; i++) {
+		all[i].value = "";
+	}
 }
 
 function username() {
-    var name = document.getElementById('username-input').value;
-    if (name) {
-        document.getElementById('change-username-button').textContent = name;
-        cancel(content, usernamecontainer);
-    } else {
-        alert("You must fill in all fields to continue.");
-    }
+	var name = document.getElementById('username-input').value;
+	if (name) {
+		document.getElementById('change-username-button').textContent = name;
+		cancel(content, usernamecontainer);
+	} else {
+		alert("You must fill in all fields to continue.");
+	}
 }
 
 function comment() // Second User Comment Template
 {
-    var name = document.getElementById('change-username-button').textContent; // Username
-    var textcontent = document.getElementById('comment-input').value; // The comment made by user
-    if (textcontent) {
-        var allcomment = check.getElementsByTagName('div')[5]; // I have already found the comment container for you
-        console.log("comment", allcomment);
-        // You need to add a comment to the post here
-        cancel(content, newcommentcontainer);
-    } else {
-        alert("You must fill all blanks!");
-    }
+	var name = document.getElementById('change-username-button').textContent; // Username
+	var textcontent = document.getElementById('comment-input').value; // The comment made by user
+	if (textcontent) {
+		var allcomment = check.getElementsByTagName('div')[5]; // I have already found the comment container for you
+		console.log("comment", allcomment);
+		// You need to add a comment to the post here
+		cancel(content, newcommentcontainer);
+	} else {
+		alert("You must fill all blanks!");
+	}
 }
 
 function dislike(ele) {
-    var number = ele.getElementsByTagName('div')[0].textContent;
-    number = parseInt(number) + 1;
-    ele.getElementsByTagName('div')[0].textContent = number;
+	var number = ele.getElementsByTagName('div')[0].textContent;
+	number = parseInt(number) + 1;
+	ele.getElementsByTagName('div')[0].textContent = number;
 }
 addpostbutton.addEventListener('click', function () {
-    show(newpostcontainer, content);
+	show(newpostcontainer, content);
 });
 
 createpostbutton.addEventListener('click', create);
 
 cancelpostbutton.addEventListener('click', function () {
-    cancel(content, newpostcontainer);
+	cancel(content, newpostcontainer);
 });
 
 usernamebutton.addEventListener('click', function () {
-    show(usernamecontainer, content);
+	show(usernamecontainer, content);
 });
 
 createuserbutton.addEventListener('click', username);
 canceluserbutton.addEventListener('click', function () {
-    cancel(content, usernamecontainer);
+	cancel(content, usernamecontainer);
 });
 
 for (var i = 0; i < addcommentbutton.length; i++) {
-    addcommentbutton[i].addEventListener('click', function () {
-        show(newcommentcontainer, content, this);
-    });
+	addcommentbutton[i].addEventListener('click', function () {
+		show(newcommentcontainer, content, this);
+	});
 }
 createcommentbutton.addEventListener('click', comment);
 cancelcommentbutton.addEventListener('click', function () {
-    cancel(content, newcommentcontainer);
+	cancel(content, newcommentcontainer);
 });
 
 for (var i = 0; i < dislikebutton.length; i++) {
-    dislikebutton[i].addEventListener('click', function () {
-        dislike(this);
-    });
+	dislikebutton[i].addEventListener('click', function () {
+		dislike(this);
+	});
 }
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ console.log("== Mongo URL:", mongoURL);
 
 
 app.engine('handlebars', exhbs({
-    defaultLayout: 'main'
+	defaultLayout: 'main'
 }));
 
 app.use(bodyParser.json());
@@ -30,39 +30,39 @@ app.use(bodyParser.json());
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    console.log("== Root page called.")
-    res.status(200).render('homePage', {
-        post: postData
-    });
+	console.log("== Root page called.")
+	res.status(200).render('homePage', {
+		post: postData
+	});
 
 });
 
 
 
 app.post('/addPost', function (req, res, res) {
-    if (req.body.postTitle && req.body.userName && req.body.postContent) {
-        console.log("Processing a new post...");
-        res.status(200).send("New post added!");
-    } else {
-        res.status(400).send("Please make sure all fields are completed and try again.");
-    }
+	if (req.body.postTitle && req.body.userName && req.body.postContent) {
+		console.log("Processing a new post...");
+		res.status(200).send("New post added!");
+	} else {
+		res.status(400).send("Please make sure all fields are completed and try again.");
+	}
 });
 
 app.use(express.static('public'));
 
 app.get('*', function (req, res) {
 
-    console.log("== 404 page called.")
+	console.log("== 404 page called.")
 
-    res.status(404).render('404Page');
+	res.status(404).render('404Page');
 });
 
 MongoClient.connect(mongoURL, function (err, connection) {
-    if (err) {
-        throw err;
-    }
-    mongoConnection = connection;
-    app.listen(port, function () {
-        console.log("== Server is listening on port: ", port);
-    });
+	if (err) {
+		throw err;
+	}
+	mongoConnection = connection;
+	app.listen(port, function () {
+		console.log("== Server is listening on port: ", port);
+	});
 });
