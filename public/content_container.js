ccaa5641591844dca1641d31aca91e34e85f53f7
buitim <buitim@oregonstate.edu>
2017-12-06 23:29:01 -0800

    Initialized MongoDB

--- public/content_container.js
+++ public/content_container.js
@@ -3,28 +3,24 @@
 templates['content_container'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return ((stack1 = container.invokePartial(partials.comment,depth0,{"name":"comment","data":data,"indent":"\t\t\t","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
+  return " "
+    + ((stack1 = container.invokePartial(partials.comment,depth0,{"name":"comment","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + " ";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<<<<<<< HEAD\r\n<div class=\"post\" data-dislikes=\"0\">\r\n    <div class=\"post-container\">\r\n        <div class=\"button-container\">\r\n            <button class=\"dislike-button\">\r\n                <div>0</div>\r\n                <div>⬇⬇️</div>\r\n            </button>\r\n            <button class=\"comment-button\">comment</button>\r\n        </div>\r\n        <div class=\"content-container\">\r\n            <h2 class=\"post-title\">"
-    + alias4(((helper = (helper = helpers.postTitle || (depth0 != null ? depth0.postTitle : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"postTitle","hash":{},"data":data}) : helper)))
-    + "</h2>\r\n            <p class=\"username\">"
-    + alias4(((helper = (helper = helpers.userName || (depth0 != null ? depth0.userName : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"userName","hash":{},"data":data}) : helper)))
-    + "</p>\r\n            <p class=\"post-content\">"
-    + alias4(((helper = (helper = helpers.postContent || (depth0 != null ? depth0.postContent : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"postContent","hash":{},"data":data}) : helper)))
-    + "</p>\r\n        </div>\r\n    </div>\r\n    <div class=\"comment-container\">\r\n    </div>\r\n</div>\r\n=======\r\n<div class=\"post\" data-dislikes=\""
+  return "<div class=\"post\" data-dislikes=\""
     + alias4(((helper = (helper = helpers.NumDislikes || (depth0 != null ? depth0.NumDislikes : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"NumDislikes","hash":{},"data":data}) : helper)))
-    + "\">\r\n	<div class=\"post-container\">\r\n		<div class=\"button-container\">\r\n			<button class=\"dislike-button\">\r\n				<div>"
+    + "\">\n    <div class=\"post-container\">\n        <div class=\"button-container\">\n            <button class=\"dislike-button\">\n                <div>"
     + alias4(((helper = (helper = helpers.NumDislikes || (depth0 != null ? depth0.NumDislikes : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"NumDislikes","hash":{},"data":data}) : helper)))
-    + "</div>\r\n				<div>⬇</div>\r\n			</button>\r\n			<button class=\"comment-button\">comment</button>\r\n		</div>\r\n		<div class=\"content-container\">\r\n			<h2 class=\"post-title\">"
+    + "</div>\n                <div>⬇</div>\n            </button>\n            <button class=\"comment-button\">comment</button>\n        </div>\n        <div class=\"content-container\">\n            <h2 class=\"post-title\">"
     + alias4(((helper = (helper = helpers.postTitle || (depth0 != null ? depth0.postTitle : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"postTitle","hash":{},"data":data}) : helper)))
-    + "</h2>\r\n			<p class=\"username\">"
+    + "</h2>\n            <p class=\"username\">"
     + alias4(((helper = (helper = helpers.userName || (depth0 != null ? depth0.userName : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"userName","hash":{},"data":data}) : helper)))
-    + "</p>\r\n			<p class=\"post-content\">"
+    + "</p>\n            <p class=\"post-content\">"
     + alias4(((helper = (helper = helpers.postContent || (depth0 != null ? depth0.postContent : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"postContent","hash":{},"data":data}) : helper)))
-    + "</p>\r\n		</div>\r\n	</div>\r\n	<div class=\"comment-container\">\r\n"
+    + "</p>\n        </div>\n    </div>\n    <div class=\"comment-container\">\n        "
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.comments : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "	</div>\r\n</div>\r\n>>>>>>> alpha\r\n";
+    + "\n    </div>\n</div>";
 },"usePartial":true,"useData":true});
 })();
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require('path');
 var express = require('express');
 var exhbs = require('express-handlebars');
 var bodyParser = require('body-parser');
-var postData = require('./postData');
+var postData = require('./data/db/postData');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -22,46 +22,46 @@ console.log("== Mongo URL:", mongoURL);
 
 
 app.engine('handlebars', exhbs({
-	defaultLayout: 'main'
+    defaultLayout: 'main'
 }));
 app.use(bodyParser.json());
 
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-	console.log("== Root page called.")
-	res.status(200).render('homePage', {
-		post: postData
-	});
+    console.log("== Root page called.")
+    res.status(200).render('homePage', {
+        post: postData
+    });
 
 });
 
 
 
 app.post('/addPost', function (req, res, res) {
-	if (req.body.postTitle && req.body.userName && req.body.postContent) {
-		console.log("Processing a new post...");
-		res.status(200).send("New post added!");
-	} else {
-		res.status(400).send("Please make sure all fields are completed and try again.");
-	}
+    if (req.body.postTitle && req.body.userName && req.body.postContent) {
+        console.log("Processing a new post...");
+        res.status(200).send("New post added!");
+    } else {
+        res.status(400).send("Please make sure all fields are completed and try again.");
+    }
 });
 
 app.use(express.static('public'));
 
 app.get('*', function (req, res) {
 
-	console.log("== 404 page called.")
+    console.log("== 404 page called.")
 
-	res.status(404).render('404Page');
+    res.status(404).render('404Page');
 });
 
 MongoClient.connect(mongoURL, function (err, connection) {
-	if (err) {
-		throw err;
-	}
-	mongoConnection = connection;
-	app.listen(port, function () {
-		console.log("== Server is listening on port: ", port);
-	});
+    if (err) {
+        throw err;
+    }
+    mongoConnection = connection;
+    app.listen(port, function () {
+        console.log("== Server is listening on port: ", port);
+    });
 });
