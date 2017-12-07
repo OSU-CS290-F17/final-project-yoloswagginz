52b779eeba7ac0e037537aa897d94af5d82e8104
buitim <buitim@oregonstate.edu>
2017-12-06 19:04:42 -0800

    Modified package.json and server.js

--- public/comment.js
+++ public/comment.js
@@ -3,18 +3,14 @@
 templates['comment'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-<<<<<<< HEAD
-  return "<div class=\"comment\">\n	<div class=\"username\">"
+  return "<<<<<<< HEAD\r\n<div class=\"comment\">\r\n	<div class=\"username\">"
     + alias4(((helper = (helper = helpers.userName || (depth0 != null ? depth0.userName : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"userName","hash":{},"data":data}) : helper)))
-    + "</div>\n	<div class=\"comment-content\">"
+    + "</div>\r\n	<div class=\"comment-content\">"
     + alias4(((helper = (helper = helpers.comment || (depth0 != null ? depth0.comment : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"comment","hash":{},"data":data}) : helper)))
-    + "</div>\n</div>";
-=======
-  return "		<div class=\"comment\">\n			<p class=\"username\">"
+    + "</div>\r\n</div>\r\n=======\r\n		<div class=\"comment\">\r\n			<p class=\"username\">"
     + alias4(((helper = (helper = helpers.commentUsername || (depth0 != null ? depth0.commentUsername : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"commentUsername","hash":{},"data":data}) : helper)))
-    + "</p>\n			<p class=\"comment-content\">"
+    + "</p>\r\n			<p class=\"comment-content\">"
     + alias4(((helper = (helper = helpers.commentContent || (depth0 != null ? depth0.commentContent : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"commentContent","hash":{},"data":data}) : helper)))
-    + "</p>\n		</div>\n";
->>>>>>> alpha
+    + "</p>\r\n		</div>\r\n>>>>>>> alpha\r\n";
 },"useData":true});
 })();
