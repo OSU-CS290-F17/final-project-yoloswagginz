ee932fb12ebad51c22034b64135321b544960a70
buitim <buitim@oregonstate.edu>
2017-12-07 15:14:37 -0800

    This is the last time we let Ryan commit ascii porn in the comments.

--- server.js
+++ server.js
@@ -3,20 +3,25 @@ var express = require('express');
 var exhbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 //var postData = require('./postData');
-var MongoClient=require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 var port = process.env.PORT || 5000;
 
-var mongoHost="classmongo.engr.oregonstate.edu";
-var mongoPort=process.env.MONGO_PORT || 27017;
-var mongoUser="cs290_chenyuha";
-var mongoPassword="cs290_chenyuha";
-var mongoDBName="cs290_chenyuha";
+// var mongoHost = "classmongo.engr.oregonstate.edu";
+// var mongoPort = process.env.MONGO_PORT || 27017;
+// var mongoUser="cs290_chenyuha";
+// var mongoPassword="cs290_chenyuha";
+// var mongoDBName="cs290_chenyuha";
 
-var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword +'@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+// MongoDB Stuff //
+var MongoClient = require('mongodb').MongoClient;
+var mongoURL = 'mongodb://localhost:27017/test'; // For sake of time and other finals, we decided to stick with localhost using local user.
+var mongoConnection = null;
 
-var mongoConnection=null;
+// var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+
+var mongoConnection = null;
 
 app.engine('handlebars', exhbs({
 	defaultLayout: 'main'
@@ -30,58 +35,60 @@ app.get('/', function (req, res) {
 	var postdatacollection = mongoConnection.collection('postData');
 	postdatacollection.find({}).toArray(function (err, results) {
 		res.status(200).render('homePage', {
-		  post: results
+			post: results
 		});
-	  }
-	);
+	});
 });
 
 
 
 app.post('/addPost', function (req, res, next) {
-	  if (req.body) {
+	if (req.body) {
 		var postdatacollection = mongoConnection.collection('postData');
 		var postobj = {
-			userName:req.body.userName,
-			NumDislikes:req.body.NumDislikes,
-			postTitle:req.body.postTitle,
-			postContent:req.body.postContent,
-			comments:[]
+			userName: req.body.userName,
+			NumDislikes: req.body.NumDislikes,
+			postTitle: req.body.postTitle,
+			postContent: req.body.postContent,
+			comments: []
 		};
-		postdatacollection.insertOne(postobj,function (err, result) {
+		postdatacollection.insertOne(postobj, function (err, result) {
 			if (err) {
-			  res.status(500).send("Error fetching people from DB");
+				res.status(500).send("Error fetching people from DB");
 			} else {
-			  res.status(200).send("Success");
+				res.status(200).send("Success");
 			}
 		});
-	
-	  } else {
+
+	} else {
 		res.status(400).send("Request body needs required field.");
-	  }
+	}
 });
 
 app.post('/addcomment', function (req, res, next) {
 	if (req.body) {
-	var text=req.body.text;
-	var postdatacollection = mongoConnection.collection('postData');
-	var postobj = {
-		commentUsername:req.body.commentUsername,
-		commentContent:req.body.commentContent
-	};
-	postdatacollection.updateOne(
-		{postTitle:text},
-		{ $push: {comments:postobj} }
-		,function (err, result) {
-		if (err) {
-			res.status(500).send("Error fetching people from DB");
-		} else {
-			res.status(200).send("Success");
-		}
-	});
+		var text = req.body.text;
+		var postdatacollection = mongoConnection.collection('postData');
+		var postobj = {
+			commentUsername: req.body.commentUsername,
+			commentContent: req.body.commentContent
+		};
+		postdatacollection.updateOne({
+			postTitle: text
+		}, {
+			$push: {
+				comments: postobj
+			}
+		}, function (err, result) {
+			if (err) {
+				res.status(500).send("Error fetching people from DB");
+			} else {
+				res.status(200).send("Success");
+			}
+		});
 
 	} else {
-	res.status(400).send("Request body needs required field.");
+		res.status(400).send("Request body needs required field.");
 	}
 });
 
@@ -96,10 +103,10 @@ app.get('*', function (req, res) {
 
 MongoClient.connect(mongoURL, function (err, connection) {
 	if (err) {
-	  throw err;
+		throw err;
 	}
 	mongoConnection = connection;
 	app.listen(port, function () {
-	  console.log("== Server listening on port:", port);
+		console.log("== Server listening on port:", port);
 	});
-  });
+});
